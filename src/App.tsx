085d4a5163b20abef9789661bb7f556807c726e3
yakovliam <jacob@yakovliam.com>
2023-08-21 15:17:24 -0400

    feat: add files

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,36 @@
+import { useEffect, useState } from "react";
+
+type IndexedFile = {
+  name: string;
+  path: string;
+  date: string;
+};
+
 function App() {
-  return <p>This is your app!</p>;
+  const [fileList, setFileList] = useState<IndexedFile[]>();
+
+  useEffect(() => {
+    fetch("/index.json")
+      .then((res) => res.json())
+      .then((data) => {
+        setFileList(data.files);
+      });
+  }, []);
+
+  return (
+    <p>
+      Welcome to resource.yakovliam.com.
+      <br />
+      This site is an index of various resources in /public.
+      <br />
+      <br />
+      {fileList?.map((file) => (
+        <a href={file.path} key={file.name}>
+          {file.name} // {file.path} // {file.date}
+        </a>
+      ))}
+    </p>
+  );
 }
 
 export default App;
